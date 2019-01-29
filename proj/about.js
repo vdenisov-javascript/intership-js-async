f63f3a817488e24bb77c5ca6344a9fa7c52a12cd
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-29 14:40:11 +0300

    create structure of project and add files for mustache

---
+++ proj/about.js
@@ -0,0 +1,10 @@
+const pathTo = {
+  mustacheTemplate: 'proj/src/template.mustache',
+  jsonWithData: 'proj/src/courseInfo.json',
+  outputHtmlFile: 'proj/public/index.html'
+}
+
+
+module.exports = {
+  pathTo,
+}
