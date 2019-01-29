d00216f9a7e8d8e2e3e1b6abc4a588448abd38d4
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-29 14:40:31 +0300

    task 2: add test

---
+++ tests/task-2.test.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const task2 = require('../tasks/task-2');
+
+
+describe('Testing "task-2" from "Javacript-Async"', () => {
+
+  const projPathTo = require('./../proj/about').pathTo;
+
+  const pathToData = path.join( __dirname, '../', projPathTo.jsonWithData );
+  const pathToTemplate = path.join( __dirname, '../', projPathTo.mustacheTemplate );
+  const pathToOutput = path.join( __dirname, '../', projPathTo.outputHtmlFile );
+
+  it('should be sure that output file ".html" not exist', () => {
+    fs.access(pathToOutput, error => {
+      if (!error) fs.unlinkSync(pathToOutput);
+    });
+
+    assert.equal( true, true );
+  });
+
+  it('should successfully convert temmplate ".mustache" to output file ".html"', () => {
+    task2.compilingViaMustache(pathToData, pathToTemplate, pathToOutput);
+
+    let success = false;
+    fs.access(pathToOutput, error => {
+      if (!error) success = true;
+    });
+
+    setTimeout(() => assert.equal( true, true ), 1000);
+  });
+
+});
