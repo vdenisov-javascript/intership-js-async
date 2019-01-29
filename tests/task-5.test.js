3beaaf96caed7ef8694df3ac27d32de327775bf2
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-29 18:17:36 +0300

    add test for task 5

---
+++ tests/task-5.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const task5 = require('../tasks/task-5');
+
+
+describe('Testing "task-5" from "Javacript-Async"', () => {
+
+  it('I can`t imagine test for it', () => {
+    const myResults = { reason: 'because "tail" just print to console' };
+
+    assert.deepEqual(true, true);
+  });
+
+});
