90b5241a0e9c36aa53ee9a68e889508189f64513
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-31 15:56:05 +0300

    fix task 3: remove promise hell

--- tasks/task-3.js
+++ tasks/task-3.js
@@ -6,39 +6,64 @@ const mustache = require('mustache');
 // ######################### //
 
 function compilingViaMustache(pathToData, pathToTemplate, pathToOutput) {
+  let view, build;
 
-  return new Promise((resolve, reject) => {
-
+  return fs.readFileAsync(pathToData, { encoding: 'utf-8' })
+  .then(json => {
     // Step 1: save data from ".json"
-    fs.readFileAsync(pathToData, { encoding: 'utf-8' })
-    .then(json => {
+    view = JSON.parse(json);
 
-      const view = JSON.parse(json);
+    // Step 2: read template from ".mustache"
+    return fs.readFileAsync(pathToTemplate, { encoding: 'utf-8' });
+  })
+  .then(template => {
+    // Step 3: parse template
+    mustache.parse(template);
 
-      // Step 2: read template from ".mustache"
-      fs.readFileAsync(pathToTemplate, { encoding: 'utf-8' })
-      .then(template => {
+    // Step 4: compile template with data
+    build = mustache.render(template, view);
 
-        // Step 3: parse template
-        mustache.parse(template);
-
-        // Step 4: compile template with data
-        const build = mustache.render(template, view);
-
-        // Step 5: save compiled result
-        fs.writeFileAsync(pathToOutput, build)
-        .catch(error => reject(error));
-
-        resolve(true);
-      })
-      .catch(error => reject(error));
-
-    })
-    .catch(error => reject(error));
-
+    // Step 5: save compiled result
+    return fs.writeFileAsync(pathToOutput, build);
   });
-
 }
+
+/** ↓↓↓ PROMISE HELL ↓↓↓ */
+
+// function compilingViaMustache(pathToData, pathToTemplate, pathToOutput) {
+
+//   return new Promise((resolve, reject) => {
+
+//     // Step 1: save data from ".json"
+//     fs.readFileAsync(pathToData, { encoding: 'utf-8' })
+//     .then(json => {
+
+//       const view = JSON.parse(json);
+
+//       // Step 2: read template from ".mustache"
+//       fs.readFileAsync(pathToTemplate, { encoding: 'utf-8' })
+//       .then(template => {
+
+//         // Step 3: parse template
+//         mustache.parse(template);
+
+//         // Step 4: compile template with data
+//         const build = mustache.render(template, view);
+
+//         // Step 5: save compiled result
+//         fs.writeFileAsync(pathToOutput, build)
+//         .catch(error => reject(error));
+
+//         resolve(true);
+//       })
+//       .catch(error => reject(error));
+
+//     })
+//     .catch(error => reject(error));
+
+//   });
+
+// }
 
 // ######################### //
 
